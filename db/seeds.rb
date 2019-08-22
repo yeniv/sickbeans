9c56f899a4742192c36edbf1f3a8284edb26a87c
Tristan Viney <yenivyeniv@gmail.com>
2019-08-23 00:27:35 +0200

    data structure hash complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,151 @@
-# create hash table of data (key = row index, value = array of values)
-
-soybean_data = {
-   1 => ['april','may','june','july','august','september','october'],
-   2 => ['normal','lt-normal'],
-   3 => ['lt-norm','norm','gt-norm'],
-   4 => ['lt-norm','norm','gt-norm'],
-   5 => ['yes','no'],
-   6 => ['diff-lst-year','same-lst-yr','same-lst-two-yr','same-lst-sev-yrs'],
-   7 => ['scattered','low-areas','upper-areas','whole-field'],
-   8 => ['minor','pot-severe','severe'],
-   9 => ['none','fungicide','other'],
-  10 => ['90-100%','80-89%','lt-80%'],
-  11 => ['norm','abnorm'],
-  12 => ['norm','abnor'],
-  13 => ['absent','yellow-halos','no-yellow-halos'],
-  14 => ['w-s-marg','no-w-s-marg','dna'],
-  15 => ['lt-1/8','gt-1/8','dna'],
-  16 => ['absent','present'],
-  17 => ['absent','present'],
-  18 => ['absent','upper-surf','lower-surf'],
-  19 => ['norm','abnorm'],
-  20 => ['yes','no'],
-  21 => ['absent','below-soil','above-soil','above-sec-nde'],
-  22 => ['dna','brown','dk-brown-blk','tan'],
-  23 => ['absent','present'],
-  24 => ['absent','firm-and-dry','watery'],
-  25 => ['absent','present'],
-  26 => ['none','brown','black'],
-  27 => ['absent','present'],
-  28 => ['norm','diseased','few-present','dna'],
-  29 => ['absent','colored','brown-w/blk-specks','distort','dna'],
-  30 => ['norm','abnorm'],
-  31 => ['absent','present'],
-  32 => ['absent','present'],
-  33 => ['norm','lt-norm'],
-  34 => ['absent','present'],
-  35 => ['norm','rotted','galls-cysts']
+data = {
+  0 => {
+    column: 'classification',
+    data: []
+  }
+  1 => {
+    column: 'date',
+    data:['april', 'may', 'june', 'july', 'august', 'september', 'october']
+  },
+  2 => {
+    column: 'plant_stand',
+    data:['normal', 'lt-normal']
+  },
+  3 => {
+    column: 'precip',
+    data:['lt-norm', 'norm', 'gt-norm']
+  },
+  4 => {
+    column: 'temp',
+    data:['lt-norm', 'norm', 'gt-norm']
+  },
+  5 => {
+    column: 'hail',
+    data:['yes', 'no']
+  },
+  6 => {
+    column: 'crop_hist',
+    data:['diff-lst-year', 'same-lst-yr', 'same-lst-two-yr', 'same-lst-sev-yrs']
+  },
+  7 => {
+    column: 'area_damaged',
+    data:['scattered', 'low-areas', 'upper-areas', 'whole-field']
+  },
+  8 => {
+    column: 'severity',
+    data:['minor', 'pot-severe', 'severe']
+  },
+  9 => {
+    column: 'seed_tmt',
+    data:['none', 'fungicide', 'other']
+  },
+  10 => {
+    column: 'germination',
+    data:['90-100%', '80-89%', 'lt-80%']
+  },
+  11 => {
+    column: 'plant_growth',
+    data:['norm', 'abnorm']
+  },
+  12 => {
+    column: 'leaves',
+    data:['norm', 'abnor']
+  },
+  13 => {
+    column: 'leafspots_halo',
+    data:['absent', 'yellow-halos', 'no-yellow-halos']
+  },
+  14 => {
+    column: 'leafspots_marg',
+    data:['w-s-marg', 'no-w-s-marg', 'dna']
+  },
+  15 => {
+    column: 'leafspot_size',
+    data:['lt-1/8', 'gt-1/8', 'dna']
+  },
+  16 => {
+    column: 'leaf_shread',
+    data:['absent', 'present']
+  },
+  17 => {
+    column: 'leaf_malf',
+    data:['absent', 'present']
+  },
+  18 => {
+    column: 'leaf_mild',
+    data:['absent', 'upper-surf', 'lower-surf']
+  },
+  19 => {
+    column: 'stem',
+    data:['norm', 'abnorm']
+  },
+  20 => {
+    column: 'lodging',
+    data:['yes', 'no']
+  },
+  21 => {
+    column: 'stem_cankers',
+    data:['absent', 'below-soil', 'above-soil', 'above-sec-nde']
+  },
+  22 => {
+    column: 'canker_lesion',
+    data:['dna', 'brown', 'dk-brown-blk', 'tan']
+  },
+  23 => {
+    column: 'fruiting_bodies',
+    data:['absent', 'present']
+  },
+  24 => {
+    column: 'external decay'
+     data:['absent', 'firm-and-dry', 'watery']
+   },
+  25 => {
+    column: 'mycelium',
+    data:['absent', 'present']
+  },
+  26 => {
+    column: 'int_discolor',
+    data:['none', 'brown', 'black']
+  },
+  27 => {
+    column: 'sclerotia',
+    data:['absent', 'present']
+  },
+  28 => {
+    column: 'fruit_pods',
+    data:['norm', 'diseased', 'few-present', 'dna']
+  },
+  29 => {
+    column: 'fruit spots'
+     data:['absent', 'colored', 'brown-w/blk-specks', 'distort', 'dna']
+   },
+  30 => {
+    column: 'seed',
+    data:['norm', 'abnorm']
+  },
+  31 => {
+    column: 'mold_growth',
+    data:['absent', 'present']
+  },
+  32 => {
+    column: 'seed_discolor',
+    data:['absent', 'present']
+  },
+  33 => {
+    column: 'seed_size',
+    data:['norm', 'lt-norm']
+  },
+  34 => {
+    column: 'shriveling',
+    data:['absent', 'present']
+  },
+  35 => {
+    column: 'roots',
+    data:['norm', 'rotted', 'galls-cysts']
+  }
 }
 
 # import data
-
-# pass data into SEED IMPORT CLASS?
-
 # loop over each line
 # loop over each item (each with index) separated by comma
 # first value = class
@@ -49,3 +153,4 @@ soybean_data = {
 # then number in value corresponds to index of the array in the hash
 # next if value = '?' (which means it will = nil)
 
+# once works, refactor into a helper class and simply pass the imported data to the class
