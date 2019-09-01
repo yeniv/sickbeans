88c4ba4b5309e8e79ea0beaa237aa331925a4b79
Tristan Viney <yenivyeniv@gmail.com>
2019-09-01 12:55:14 +0200

    comment cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,14 @@ class PagesController < ApplicationController
   def index
     @soybeans = Bean.all
     @collection = Bean.form_collection
-
     search = params['search']
 
     if search.present?
       @soybeans = Bean.where.not(Bean.params_to_hash(search))
-
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js
       end
-
     end
   end
 end
